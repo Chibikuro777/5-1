ead0fdec3e8dbb66b6f17b705489081192b0325e
Chibikuro777 <56582396+Chibikuro777@users.noreply.github.com>
2022-04-19 17:17:14 +0900

    途中経過

---
+++ database/migrations/2022_04_07_092110_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigInteger('id');
+            $table->bigInteger('user_id')->nullable();
+            $table->string('body', 255)->nullable();
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center mb-2">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">ホーム</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                <form action="" method="get">
+                    @csrf
+                    <input type="text" name="" placeholder="いまどうしてる？" class="w-100 mb-4 p-1">
+                    <div class="d-flex justify-content-end"><button name="button" class="btn btn-secondary">つぶやく</button></div>
+                </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    content
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
